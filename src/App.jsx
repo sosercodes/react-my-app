349bf39a6dbb77be9f03dc7318a7fab083730ff5
Simon Oser <simon.oser@gmail.com>
2024-09-29 17:21:58 +0200

    Handling Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  var books = [
+    {id: 1, author: "Marijn Haverbeke", title:"Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming"},
+    {id: 2, author: "Robert C. Martin", title:"Clean Code: A Handbook of Agile Software Craftsmanship"},
+    {id: 3, author: "Maximilian Schwarzmüller", title:"React Key Concepts: Consolidate your knowledge of React's core features"},
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>React Lists</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ul>
+          {
+          books.map((item) =>
+            <li key={item.id}><strong>{item.author}:</strong> {item.title}</li>)
+          }
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
