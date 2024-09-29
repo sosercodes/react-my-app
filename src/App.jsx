3c02d4db604e3422da5993ffd9c4cf345f402152
Simon Oser <simon.oser@gmail.com>
2024-09-29 10:08:41 +0200

    uses useEffect to show message after render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('Hello from useEffect')
+  });
 
   return (
     <>
